7fce0c16f8d478070e2418961fbd59a72a78ea47
BorisBresler <9bres0@gmail.com>
2020-04-17 22:30:18 +0500

    dobavil flex

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-// добавление текста который написали в гостевой книге.
-$(function () {
-  $("#buttonguest").click(function () {
-    $.get(
-      "http://data.fixer.io/api/latest",
-      { acces_key: "7556a4a9e24d262b95e2bdc7193a5b65" },
-      function (response) {
-        console.log(response);
-      }
-    );
-  });
+let buttonGueset = document.querySelector("#buttonguest");
+buttonGueset.addEventListener("click", function () {
+  alert("ti pidor");
 });
